d801a82f01ed44be1bbe849c25c8bd49eff6679c
Daniel Černý <dandul.black@gmail.com>
2025-03-09 21:48:48 +0100

    hotovo, počítá a bere to i chyby

--- calculator/index.js
+++ calculator/index.js
@@ -1,10 +1,22 @@
 const bar = document.getElementById("calculator-bar-id");
 console.log(bar);
+let reset = false;
 
 function calculator(num) {
-    if (bar.innerText == "klikněte na tlačítka") {
+    if (bar.innerText == "klikněte na tlačítka" || reset) {
         bar.innerText = num;
+        reset = false;
     } else {
         bar.innerText += num;
+    }
+}
+
+function výsledek() {
+    try {
+        bar.innerText = eval(bar.innerText);
+        reset = true;
+    } catch {
+        bar.innerText = "chyba";
+        reset = true;
     }
 }
